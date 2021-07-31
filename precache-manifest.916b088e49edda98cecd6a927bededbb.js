3108cb9c94dfc944041d496751bbb67b67d25b7d
Matano <mwakimamatano@gmail.com>
2021-07-31 09:31:36 +0300

    deploy

---
+++ precache-manifest.916b088e49edda98cecd6a927bededbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a1968f7d1ca4dd0422",
+    "url": "/Clock-app/css/app.2a68648d.css"
+  },
+  {
+    "revision": "79bcfa4daf6a631472fa20c3a60daef8",
+    "url": "/Clock-app/index.html"
+  },
+  {
+    "revision": "07a1968f7d1ca4dd0422",
+    "url": "/Clock-app/js/app.e692ea91.js"
+  },
+  {
+    "revision": "f4e1bae969ce1ddd9663",
+    "url": "/Clock-app/js/chunk-vendors.f369bbb8.js"
+  },
+  {
+    "revision": "e35bba74b1818eb5382d0ae8e27b9caa",
+    "url": "/Clock-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Clock-app/robots.txt"
+  }
+]);
